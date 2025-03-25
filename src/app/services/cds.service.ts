179c91e74b619b79c963501b380115100a64be3e
Candice Dutournier <cdutourni001@iutbayonne.univ-pau.fr>
2025-03-25 18:31:23 +0100

    début api

--- src/app/services/cds.service.ts
+++ src/app/services/cds.service.ts
@@ -1,44 +1,17 @@
 import { Injectable } from '@angular/core';
 import { CD } from '../models/cd.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CdsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getCds(): CD[]{
+  getCds(): Observable<CD[]>{
     return [
-      {
-        id : 1,
-        title : 'The DarkSide of the Moon',
-        author : 'Pink Floyd',
-        price : 10,
-        thumbnail : 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/The_Dark_Side_of_the_Moon_Cover.svg/220px-The_Dark_Side_of_the_Moon_Cover.svg.png',
-        dateDeSortie : new Date (1973,3,1),
-        quantite : 1
-      },
-      {
-        id : 2,
-        title : 'Pulse',
-        author : 'Pink Floyd',
-        price : 10,
-        thumbnail : 'https://pinkfloydhyperbase.dk/illu/covers/pulse.jpg',
-        dateDeSortie : new Date (1974,4,11),
-        quantite : 2,
-        onsale : true
-      },
-      {
-        id : 3,
-        title : 'Pulse1',
-        author : 'Pink Floyd',
-        price : 100,
-        thumbnail : 'https://pinkfloydhyperbase.dk/illu/covers/pulse.jpg',
-        dateDeSortie : new Date (1974,6,20),
-        quantite : 0
-      }
-    ];
+    ]
   }
 
   getCDById(id: number): CD {
